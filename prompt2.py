d94e5c68cbaed8df87ee453c2d754d5db1110363
ey3von <146590411+ey3von@users.noreply.github.com>
2023-10-26 13:02:33 -0700

    Add files via upload

---
+++ prompt2.py
@@ -0,0 +1,20 @@
+#Run a python program that prints the sum of two floating numbers
+
+x = 762.0 
+y = 14.0
+
+print("x + y =", x+y)
+
+#difference between two integers
+
+x = 762
+y = 14
+
+print("x - y =", x-y)
+
+#product of a floating number and an integer
+
+x = 762
+y = 14.0 
+
+print("x * y +", x*y)
